e22a11f94b9009d3db229bdf1d45df10b6ba9d88
Seth Dorris <Seth@Seths-MBP.attlocal.net>
2015-10-07 17:01:47 -0500

    sliding menu complete

--- controllers/navcontroller.js
+++ controllers/navcontroller.js
@@ -1,41 +1,17 @@
-main.controller("navcontroller", ["$scope", "$firebaseAuth", "$location", "Auth", function ($scope, $firebaseAuth, $location, Auth) {
+main.controller("navcontroller", function ($scope) {
 	
-	var ref = new Firebase("https://gtxtfantasy.firebaseio.com");
-	
-//Checks to see if a user is authenticated, if user isn't authenticated, $scope.Auth set to false
-//If user is authenticated and logged in, $scope.Auth is true and then userName is generated
+	$scope.menuButton = function () {
+		$('#menuBtn').hide();
+		$("nav > ul").removeClass('closed');
+		$("nav > ul").addClass('active');
 
-	if (!ref.getAuth()) {
-		$scope.Auth = false
-		$location.path("/")
-	} else {
-		$scope.userName = ref.getAuth();
-		$scope.userName = $scope.userName.facebook.displayName;
-		$scope.userName = $scope.userName.split(" ");
-		$scope.userName = $scope.userName[0];
-		$scope.Auth = true;
 	}
 
-	$scope.logout = function () {
-		ref.unauth();
-		$scope.Auth = false;
-		$scope.userName = "";
-		$location.path("/");
-	};
+	$scope.closeNav = function () {
+		$('nav > ul').removeClass('active');
+		$('nav > ul').addClass('closed');
+		$('#menuBtn').show();
+	}
+	
 
-	$scope.login = function () {
-		//Calls the setUid function in Auth, which is the AUTH POPUP
-		var promise = Auth.setUid();
-		promise.then(function () {
-			$scope.userName = ref.getAuth();
-			$scope.userName = $scope.userName.facebook.displayName;
-			$scope.userName = $scope.userName.split(" ");
-			$scope.userName = $scope.userName[0];
-			$scope.Auth = true;
-			//Code to redirect
-			$location.path("/callback");
-
-		});
-	};
-
-}]);  //This is the end of the Controller
+});  //This is the end of the Controller
