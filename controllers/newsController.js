eb96ed3b5db850f0013efc79c18ec183db312471
sethdorris <sethc.dorris@gmail.com>
2016-04-01 11:00:51 -0600

    Initial functionality of posting message to server and reading back

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,6 +1,18 @@
 main.controller("newsController", ["$scope", "$firebaseArray", function ($scope, $firebaseArray) {
 	var ref = new Firebase("https://siege.firebaseio.com/news");
+	var messageRef = new Firebase("https://siege.firebaseio.com/messages");
 	$scope.news = $firebaseArray(ref);
+	$scope.messages = $firebaseArray(messageRef);
 	console.log("news", $scope.news);
+	
+	$scope.postMessage = function() {
+		console.log("Firing")
+		var username = $("#chatboxUsername").val();
+		var messagecontent = $("#chatboxMessage").val();
+		$scope.messages.$add({
+			username: username,
+			message: messagecontent
+		});
+	}
 
 }]);  //This is the end of the Controller
